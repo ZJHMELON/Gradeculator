c89ff331159eb5dd5bcb3e241e1cf7e73ec2a668
weng218 <weng218@gmail.com>
2017-04-28 16:40:14 -0400

    MainActivity created newIntent, created ReminderBackground Service

--- app/src/main/java/edu/umd/cs/gradeculator/MainActivity.java
+++ app/src/main/java/edu/umd/cs/gradeculator/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.umd.cs.gradeculator;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class MainActivity extends SingleFragmentActivity {
@@ -7,6 +9,11 @@ public class MainActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return MainFragment.newInstance();
+    }
+
+    public static Intent newIntent(Context context){
+        Intent intent = new Intent(context, MainActivity.class);
+        return intent;
     }
 
 }
